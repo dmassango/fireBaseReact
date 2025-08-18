74902e870b595efc24b91fa2c83db9bc03295fda
dario massango <dariodimas.massango@cedsif.gov.mz>
2025-08-18 14:40:24 +0200

    New

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { db } from './firebaseConnection';
+import './app.css'
+import { useState } from 'react';
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
 
 function App() {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd(){
+    /*await setDoc(doc(db, "posts", "12345"), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(()=>{
+      console.log("DADOS REGISTRADOS NO BANCO")
+    })
+    .catch((error)=>{
+      console.log("GERROU ERRO" + error)
+    })*/
+
+    await addDoc (collection(db, "posts"),{
+      titulo: titulo,
+      autor: autor,
+    }).then(()=>{
+      console.log("DADOS REGISTRADOS NO BANCO")
+      setTitulo('');
+      setAutor('');
+    }).catch((error)=>{
+      console.log("GERROU ERRO" + error)
+    })
+  }
+
+  async function handleGet(){
+
+      const postRef = doc(db, "posts", "12345")
+
+      await getDoc(postRef)
+    .then((snapshot)=>{
+      setTitulo(snapshot.data().autor)
+      setAutor(snapshot.data().titulo)
+    }).catch(()=>{
+      console.log("GERROU ERRO")
+    })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>ReactJS + Firebase :)</h1>
+
+        <div className='container'>
+          <label>Titulo:</label>
+            <textarea 
+              type='text' 
+              placeholder='Digite o titulo'
+              value={titulo}
+              onChange={(e)=> setTitulo(e.target.value)}
+            />
+
+          <label>Autor:</label>
+            <input
+              type='text' 
+              placeholder='Autor do post'
+              value={autor}
+              onChange={(e)=> setAutor(e.target.value)}
+            />
+
+          <button onClick={handleAdd}>Cadastrar</button>
+          <button onClick={handleGet}>Buscar Post</button>
+        </div>
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4RrN8DMHgjB2ZaTdWZhT1O-9hF0OgNJk",
+  authDomain: "curso-78166.firebaseapp.com",
+  projectId: "curso-78166",
+  storageBucket: "curso-78166.firebasestorage.app",
+  messagingSenderId: "850719955074",
+  appId: "1:850719955074:web:2b5afbe8ae8d0473d57c9b",
+  measurementId: "G-9060TJGN36"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export{ db };
