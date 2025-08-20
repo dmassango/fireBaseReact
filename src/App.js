080478a268bf06dc1dd5a205b9bb3303a13b3f6b
dario massango <dariodimas.massango@cedsif.gov.mz>
2025-08-20 09:56:12 +0200

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { db } from './firebaseConnection';
 import './app.css'
 import { useState } from 'react';
-import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc} from 'firebase/firestore';
 
 function App() {
 
   const [titulo, setTitulo] = useState('');
   const [autor, setAutor] = useState('');
+  const [idPost, setIdPost] = useState('');
+  const [posts, setPosts] = useState([]);
 
   async function handleAdd(){
     /*await setDoc(doc(db, "posts", "12345"), {
@@ -30,11 +32,13 @@ function App() {
     }).catch((error)=>{
       console.log("GERROU ERRO" + error)
     })
+
+    handleGet();
   }
 
   async function handleGet(){
 
-      const postRef = doc(db, "posts", "12345")
+     /* const postRef = doc(db, "posts", "1234")
 
       await getDoc(postRef)
     .then((snapshot)=>{
@@ -42,14 +46,64 @@ function App() {
       setAutor(snapshot.data().titulo)
     }).catch(()=>{
       console.log("GERROU ERRO")
-    })
+    })*/
 
-  }
+    const postRef = collection(db, "posts")
+    await getDocs(postRef) 
+    .then((snapshot)=>{
+      let lista = [];
+
+      snapshot.forEach((doc) => { 
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+     setPosts(lista);
+
+  })
+  .catch(()=>{
+    console.log("GERROU ERRO")
+  })
+}
+
+async function handleUpdate(){
+  /*alert("Funciona");*/
+  const docRef = doc(db, "posts", idPost)
+
+  await updateDoc (docRef, {
+    titulo: titulo,
+    autor: autor
+  })
+  .then(()=>{
+    console.log("POST ACTUALIZADO")
+    setTitulo('');
+      setAutor('');
+      setIdPost('');
+  })
+  .catch(()=>{
+    console.log("GERROU ERRO")
+  })
+
+  handleGet();
+}
+
+
   return (
     <div className="App">
-        <h1>ReactJS + Firebase :)</h1>
+        <h1>ReactJS + Firebase :-)</h1>
 
         <div className='container'>
+
+        <label>Id do Post:</label>
+            <input
+              placeholder='Digite ID do Post'
+              value={idPost}
+              onChange={(e)=> setIdPost(e.target.value)}
+            />
+
           <label>Titulo:</label>
             <textarea 
               type='text' 
@@ -67,7 +121,21 @@ function App() {
             />
 
           <button onClick={handleAdd}>Cadastrar</button>
-          <button onClick={handleGet}>Buscar Post</button>
+          <button onClick={handleGet}>Buscar Lista</button>
+          <button onClick={handleUpdate}>Actualizar</button>
+
+            <ul>
+              {posts.map((post)=>{
+                  return(
+                    <li key={post.id}>
+                    <strong>ID: {post.id}</strong><br/>
+                      <span>Titulo: {post.titulo}</span><br/>
+                      <span>Autor: {post.autor}</span><br/><br/>
+                    </li>
+                  )
+              })}
+            </ul>
+
         </div>
     </div>
   );
